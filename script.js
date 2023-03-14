e7a5a368b11bea4965554217ea80d512d360f18a
Gigi <v4vdoo@gmail.com>
2023-03-14 11:01:17 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        // navText: ['','']
+        autoplay: true,
+        autoplayTimeout: 6000,
+    });
+  });
